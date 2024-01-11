3f6b5fc7df2a18171b869def3cdefdfe8e867040
k.alshannikava <olshannikovastw@gmail.com>
2024-01-11 10:22:37 +0100

    Remove async keyword as it's not needed anymore

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import app from './app';
 const PORT: number = Number(process.env.PORT) || 8000;
 const server: Server = http.createServer(app);
 
-server.listen(PORT, async () => {
+server.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`)
 });
