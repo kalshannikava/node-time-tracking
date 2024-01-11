c2f3a21884d4fdf30ef84d8639a42ac2782312ef
k.alshannikava <olshannikovastw@gmail.com>
2024-01-11 10:42:35 +0100

    Add usersRouter

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Express } from 'express';
+import usersRouter from './routes/users.router';
 
 const app: Express = express();
 
 app.use(express.json()); // parse body to json
+app.use('/users', usersRouter);
 
 export default app;
