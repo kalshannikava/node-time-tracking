4e1b8b0324457e383205a137098d951a0530aa70
k.alshannikava <olshannikovastw@gmail.com>
2024-01-12 16:19:10 +0100

    Create middleware functions for requests validation

---
+++ src/middleware/users.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { validate } from 'email-validator';
+import db from '../db';
+import { RequestWithID } from '../types/user';
+
+async function checkIfUserExists (req: RequestWithID, res: Response, next: NextFunction) {
+  const id: number = Number(req.params.id);
+  const index: number = await db.getIndex('/users', id, 'id');
+  if (index === -1) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+  req.index = index;
+  next();
+}
+
+function validateEmail (req: Request, res: Response, next: NextFunction) {
+  if (req.body.email && !validate(req.body.email)) {
+    return res.status(400).json({ error: 'Invalid email' });
+  }
+  next();
+}
+
+export {
+  checkIfUserExists,
+  validateEmail,
+}
