f0bf5de567f82cc515239eaeabba5b14c09455f1
k.alshannikava <olshannikovastw@gmail.com>
2024-01-12 15:32:09 +0100

    Add types for requests

--- src/types/team.ts
+++ src/types/team.ts
@@ -1,5 +1,19 @@
+import { Request } from 'express';
+
 export type Team = {
   id: number,
   name: string,
   logo: string,
 }
+
+export interface GetTeamRequest extends Request<{ id: string }> {}
+
+export interface CreateTeamRequest extends Request {
+  body: Omit<Team, 'id'>;
+}
+
+export interface UpdateTeamRequest extends Request<{ id: string }> {
+  body: Partial<Omit<Team, 'id'>>;
+}
+
+export interface DeleteTeamRequest extends Request<{ id: string }> {}
