5a11be92cc17b9fde09678ac3b6f407d59a2811e
k.alshannikava <olshannikovastw@gmail.com>
2024-01-12 16:19:38 +0100

    Refactor - use middleware to validate requests

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { validate } from 'email-validator';
 
 import db from '../db';
 import { CreateUserRequest, DeleteUserRequest, GetUserRequest, UpdateUserRequest, User } from '../types/user';
@@ -10,12 +9,7 @@ async function getUsers (_req: Request, res: Response) {
 }
 
 async function getUser (req: GetUserRequest, res: Response) {
-  const id: number = Number(req.params.id);
-  const index: number = await db.getIndex('/users', id, 'id');
-  if (index === -1) {
-    return res.status(404).json({ error: 'User not found' });
-  }
-  const data: User = await db.getObject<User>(`/users[${index}]`);
+  const data: User = await db.getObject<User>(`/users[${req.index}]`);
   return res.status(200).json(data);
 }
 
@@ -28,42 +22,26 @@ async function createUser (req: CreateUserRequest, res: Response) {
   if (!user.email || !user.name || !user.timezone) {
     return res.status(400).json({ error: 'Missing required property' });
   }
-  if (!validate(user.email)) {
-    return res.status(400).json({ error: 'Invalid email' });
-  }
   await db.push('/users[]', user);
   return res.status(201).json(user);
 }
 
 async function deleteUser (req: DeleteUserRequest, res: Response) {
-  const id: number = Number(req.params.id);
-  const index: number = await db.getIndex('/users', id, 'id');
-  if (index === -1) {
-    return res.status(404).json({ error: 'User not found' });
-  }
-  const deleted: User = await db.getObject<User>(`/users[${index}]`);
-  await db.delete(`/users[${index}]`);
+  const deleted: User = await db.getObject<User>(`/users[${req.index}]`);
+  await db.delete(`/users[${req.index}]`);
   return res.status(200).json(deleted);
 }
 
 async function updateUser (req: UpdateUserRequest, res: Response) {
-  const id: number = Number(req.params.id);
-  const index: number = await db.getIndex('/users', id, 'id');
-  if (index === -1) {
-    return res.status(404).json({ error: 'User not found' });
-  }
   const { name, email, timezone } = req.body;
-  if (email && !validate(email)) {
-    return res.status(400).json({ error: 'Invalid email' });
-  }
-  const user: User = await db.getObject<User>(`/users[${index}]`);
+  const user: User = await db.getObject<User>(`/users[${req.index}]`);
   const updatedUser: User = {
     ...user,
     ...name && { name },
     ...email && { email },
     ...timezone && { timezone },
   };
-  await db.push(`/users[${index}]`, updatedUser, true); // 3rd param is set to override the data
+  await db.push(`/users[${req.index}]`, updatedUser, true); // 3rd param is set to override the data
   res.status(200).json(updatedUser);
 }
 
