19afecd006ab32417e245a854e59f4ee37301a8a
k.alshannikava <olshannikovastw@gmail.com>
2024-01-16 14:57:27 +0100

    Use UserService inside of controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,47 +1,33 @@
 import { Request, Response } from 'express';
 
-import DataBase from '../db';
-import { CreateUserRequest, DeleteUserRequest, GetUserRequest, UpdateUserRequest, User } from '../types/user';
+import UserService from '../services/users.service';
+import type { CreateUserRequest, DeleteUserRequest, GetUserRequest, UpdateUserRequest, User } from '../types/user';
 
 async function getUsers (_req: Request, res: Response) {
-  const data: User[] = await DataBase.getUsers();
+  const data: User[] = await UserService.getUsers();
   return res.status(200).json(data);
 }
 
 async function getUser (req: GetUserRequest, res: Response) {
-  // const data: User = await DataBase.getUser(req.index);
   return res.status(200).json(req.user);
 }
 
 async function createUser (req: CreateUserRequest, res: Response) {
-  const lastUser: User = await DataBase.getUser(-1);
-  const user: User = {
-    ...req.body,
-    id: lastUser.id + 1,
-  };
-  if (!user.email || !user.name || !user.timezone) {
-    return res.status(400).json({ error: 'Missing required property' });
+  try {
+    const user: User = await UserService.createUser(req.body);
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
   }
-  await DataBase.addUser(user);
-  return res.status(201).json(user);
 }
 
 async function deleteUser (req: DeleteUserRequest, res: Response) {
-  // const deleted: User = await DataBase.getUser(req.index);
-  await DataBase.deleteUser(req.index)
+  await UserService.deleteUser(req.index);
   return res.status(200).json(req.user);
 }
 
 async function updateUser (req: UpdateUserRequest, res: Response) {
-  const { name, email, timezone } = req.body;
-  // const user: User = await DataBase.getUser(req.index);
-  const updatedUser: User = {
-    ...req.user,
-    ...name && { name },
-    ...email && { email },
-    ...timezone && { timezone },
-  };
-  await DataBase.updateUser(req.index, updatedUser);
+  const updatedUser: User = await UserService.updateUser(req.index, req.user, req.body);
   res.status(200).json(updatedUser);
 }
 
