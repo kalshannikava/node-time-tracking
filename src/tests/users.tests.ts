ff9417812ab9d057429389787925155db0eebd36
k.alshannikava <olshannikovastw@gmail.com>
2024-01-25 10:35:38 +0100

    Update tests

--- src/tests/users.tests.ts
+++ src/tests/users.tests.ts
@@ -1,19 +1,22 @@
 import request from 'supertest';
+import type { Express } from 'express';
+import { join } from 'path';
 import app from '../app';
 import { User } from '../types/user';
 import DataBase from '../db';
 
 let initialDb: User[] = null;
 
-beforeAll(async () => {
-  DataBase.getInstance();
-  initialDb = [...await DataBase.getInstance().getUsers()]
-});
-afterAll(async () => await DataBase.getInstance().writeUsers(initialDb));
+const filename: string = join(__dirname, '..', 'db', 'db.json');
+const db: DataBase = new DataBase(filename);
+const application: Express = app(db);
+
+beforeAll(async () => initialDb = [...await db.getAll<User>('users')]);
+afterAll(async () => await db.writeAll<User>('users', initialDb));
 
 describe('GET /users', () => {
   it('should respond with 200 success', async () => {
-    await request(app).get('/users')
+    await request(application).get('/users')
     .expect('Content-Type', /json/)
     .expect(200);
   });
@@ -21,41 +24,41 @@ describe('GET /users', () => {
 
 describe('GET /users/id', () => {
   it('should respond with 200 success', async () => {
-    await request(app).get('/users/0')
+    await request(application).get('/users/0')
     .expect('Content-Type', /json/)
     .expect(200);
   });
 
   it('should respond with 404 not found when user doesn\'t exist', async () => {
-    await request(app).get('/users/-1')
+    await request(application).get('/users/-1')
     .expect(404);
   });
 });
 
 describe('POST /users', () => {
   it('should respond with 201 created', async () => {
-    await request(app).post('/users')
+    await request(application).post('/users')
     .send({ name: 'Test', email: 'test@test.com', timezone: 'GMT+1' })
     .expect('Content-Type', /json/)
     .expect(201);
   });
 
   it('should respond with 400 bad request when required property is missing', async () => {
-    await request(app).post('/users')
+    await request(application).post('/users')
     .send({ name: 'Test', email: 'test@test.com' })
     .expect(400);
 
-    await request(app).post('/users')
+    await request(application).post('/users')
     .send({ name: 'Test', timezone: 'GMT+1' })
     .expect(400);
 
-    await request(app).post('/users')
+    await request(application).post('/users')
     .send({ email: 'test@test.com', timezone: 'GMT+1' })
     .expect(400);
   });
 
   it('should respond with 400 bad request when email is invalid', async () => {
-    await request(app).post('/users')
+    await request(application).post('/users')
     .send({ name: 'Test', email: 'invalid email', timezone: 'GMT+1' })
     .expect(400);
   });
@@ -63,14 +66,14 @@ describe('POST /users', () => {
 
 describe('PUT /user', () => {
   it('should respond with 200 success', async () => {
-    await request(app).put('/users/0')
+    await request(application).put('/users/0')
     .send({ name: 'Test 2', email: 'test2@test.com', timezone: 'GMT+2' })
     .expect('Content-Type', /json/)
     .expect(200);
   });
 
   it('should have been updated', async () => {
-    const response = await request(app).get('/users/0')
+    const response = await request(application).get('/users/0')
     .expect('Content-Type', /json/)
     .expect(200);
     expect(response.body.name).toEqual('Test 2');
@@ -81,18 +84,18 @@ describe('PUT /user', () => {
 
 describe('DELETE /user', () => {
   it('should respond with 200 success', async () => {
-    await request(app).delete('/users/0')
+    await request(application).delete('/users/0')
     .expect('Content-Type', /json/)
     .expect(200);
   });
 
   it('should be deleted', async () => {
-    await request(app).get('/users/0')
+    await request(application).get('/users/0')
     .expect(404);
   });
 
   it('should respond with 404 not found when user doesn\'t exist', async () => {
-    await request(app).delete('/users/-1')
+    await request(application).delete('/users/-1')
     .expect(404);
   });
 });
