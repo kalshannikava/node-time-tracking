5d87d77d5c5d6d44a3ab982692130d9354bfe532
k.alshannikava <olshannikovastw@gmail.com>
2024-01-25 10:35:31 +0100

    Update app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
 import express, { Express } from 'express';
-import DataBase from './db';
+
 import UserService from './services/users.service';
 import usersRouter from './routes/users.router';
 import UsersMiddleware from './middleware/users.middleware';
 import UsersController from './controllers/users.controller';
+import UserRepository from './repositories/userRepository';
+import type DataBase from './db';
 
-const app: Express = express();
+function app (db: DataBase): Express {
+  const application: Express = express();
 
-const db: DataBase = DataBase.getInstance();
+  const userRepository: UserRepository = new UserRepository(db);
+  const userService: UserService = new UserService(userRepository);
+  const usersMiddleware: UsersMiddleware = new UsersMiddleware(userRepository);
+  const usersController: UsersController = new UsersController(userService);
 
-const userService: UserService = new UserService(db);
-const usersMiddleware: UsersMiddleware = new UsersMiddleware(db);
-const usersController: UsersController = new UsersController(userService);
+  application.use(express.json()); // parse body to json
+  application.use('/users', usersRouter(usersController, usersMiddleware));
 
-app.use(express.json()); // parse body to json
-app.use('/users', usersRouter(usersController, usersMiddleware));
+  return application;
+}
 
 export default app;
