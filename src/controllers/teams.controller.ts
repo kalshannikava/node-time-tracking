6a26aeee33609ffc4414a100f0d9f833e6aece6c
k.alshannikava <olshannikovastw@gmail.com>
2024-02-12 15:12:18 +0100

    Update controllers

--- src/controllers/teams.controller.ts
+++ src/controllers/teams.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 import type TeamsService from '../services/teams.service';
-import type { CreateTeamRequest, DeleteTeamRequest, GetTeamRequest, Team, UpdateTeamRequest } from '../types/team';
+import type { CreateTeamRequest, DeleteTeamRequest, GetTeamRequest, UpdateTeamRequest } from '../types/team';
 
 type Config = {
   teamsService: TeamsService,
@@ -15,7 +15,7 @@ class TeamsController {
   }
 
   public async getTeams (_req: Request, res: Response) {
-    const data: Team[] = await this.teamsService.getAll();
+    const data = await this.teamsService.getAll();
     return res.status(200).json(data);
   }
 
@@ -25,7 +25,7 @@ class TeamsController {
 
   public async createTeam (req: CreateTeamRequest, res: Response) {
     try {
-      const team: Team = await this.teamsService.create(req.body);
+      const team = await this.teamsService.create(req.body);
       return res.status(201).json(team);
     } catch (error) {
       return res.status(400).json({ error: error.message });
@@ -33,12 +33,12 @@ class TeamsController {
   }
 
   public async updateTeam (req: UpdateTeamRequest, res: Response) {
-    const updatedTeam: Team = await this.teamsService.update(req.index, req.entity as Team, req.body);
+    const updatedTeam = await this.teamsService.update(Number(req.params.id), req.body);
     res.status(200).json(updatedTeam);
   }
 
   public async deleteTeam (req: DeleteTeamRequest, res: Response) {
-    await this.teamsService.delete(req.index);
+    await this.teamsService.delete(Number(req.params.id));
     return res.status(200).json(req.entity);
   }
 }
