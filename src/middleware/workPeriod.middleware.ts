4d59f1dad5f49f9ee4061dc0ddbe91bf8073c5ce
k.alshannikava <olshannikovastw@gmail.com>
2024-02-13 14:12:31 +0100

    Update middleware

--- src/middleware/workPeriod.middleware.ts
+++ src/middleware/workPeriod.middleware.ts
@@ -1,13 +1,10 @@
 import { Response, NextFunction } from 'express';
 
 import type { CreateWorkPeriodRequest, UpdateWorkPeriodRequest } from '../types/workPeriod';
-import type { WorkPeriod } from '../entity/WorkPeriod';
 import type { RequestWithID } from '../types/shared';
 import type WorkPeriodsRepository from '../repositories/workPeriods.repository';
 import type UsersRepository from '../repositories/users.repository';
 import type TeamsRepository from '../repositories/teams.repository';
-import { User } from '../entity/User';
-import { Team } from '../entity/Team';
 
 type WorkPeriodsMiddlewareContext = {
   workPeriodsRepository: WorkPeriodsRepository,
@@ -25,9 +22,10 @@ class WorkPeriodsMiddleware {
     this.usersRepository = usersRepository;
     this.teamsRepository = teamsRepository;
   }
+
   public async checkIfWorkPeriodExists (req: RequestWithID, res: Response, next: NextFunction) {
-    const id: number = Number(req.params.id);
-    const workPeriod: WorkPeriod = await this.workPeriodsRepository.get(id);
+    const id = Number(req.params.id);
+    const workPeriod = await this.workPeriodsRepository.get(id);
     if (!workPeriod) {
       return res.status(404).json({ error: 'Work period not found' });
     }
@@ -36,17 +34,26 @@ class WorkPeriodsMiddleware {
   }
 
   public async checkIfTeamOrUserExist (req: CreateWorkPeriodRequest | UpdateWorkPeriodRequest, res: Response, next: NextFunction) {
-    const teamId: number = Number(req.body.teamId);
-    const userId: number = Number(req.body.userId);
-    const user: User = await this.usersRepository.get(userId);
+    const teamId = Number(req.body.teamId);
+    const userId = Number(req.body.userId);
+
+    if (Number.isNaN(userId)) {
+      return res.status(404).json({ error: 'Invalid user input: expected number' });
+    }
+
+    if (Number.isNaN(teamId)) {
+      return res.status(404).json({ error: 'Invalid team input: expected number' });
+    }
+
+    const user = await this.usersRepository.get(userId);
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    const team: Team = await this.teamsRepository.get(teamId);
+    const team = await this.teamsRepository.get(teamId);
     if (team) {
       return res.status(404).json({ error: 'Team not found' });
-    }
+    } 
     next();
   }
 }
