0eda00e7c3e141ab79602ae845fcad9a1152b759
k.alshannikava <olshannikovastw@gmail.com>
2024-02-26 13:19:35 +0100

    Add Session entity

---
+++ src/entities/Session.entity.ts
@@ -0,0 +1,18 @@
+import { ISession } from 'connect-typeorm';
+import { Column, DeleteDateColumn, Entity, Index, PrimaryColumn } from 'typeorm';
+
+@Entity('session')
+export class Session implements ISession {
+  @Index()
+  @Column('bigint')
+  expiredAt: number;
+
+  @PrimaryColumn('text')
+  id: string;
+
+  @Column('text')
+  json: string;
+
+  @DeleteDateColumn()
+  public destroyedAt?: Date;
+}
